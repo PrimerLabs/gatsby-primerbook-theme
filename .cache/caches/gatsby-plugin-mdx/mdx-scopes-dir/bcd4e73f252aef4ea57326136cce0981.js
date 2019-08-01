55daed015d79fe325a339792d6c8809fa96853bb
Siddharth Kanungo <sid.dharth.soka@gmail.com>
2019-08-01 12:32:45 +0530

    Added Readme

--- .cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/bcd4e73f252aef4ea57326136cce0981.js
+++
@@ -1,9 +0,0 @@
-import CardSystem, { Card } from "../../../../src/components/FlashCard.jsx";
-import Card1 from "../../../../content/cards/card1.mdx";
-import React from 'react';
-export default {
-  CardSystem,
-  Card,
-  Card1,
-  React
-};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Gatsby Primer Book Theme
+
+Hi, this is not quite ready. Please check out a little bit later. 
+
+Thanks
